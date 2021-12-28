6b8bd0b38291aa8a46dcfd39c228d4eecf3240a6
lbertoncello <lucas_b_andrade@hotmail.com>
2021-12-28 20:59:28 -0300

    class: component styles and global styles

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="backdrop">
     <div class="modal">
+      <h1>Modal title</h1>
       <p>Modal content</p>
     </div>
   </div>
 </template>
 
-<style>
+<!-- scoped causes all styles to be applied only to the specified component -->
+<style scoped>
   .modal {
     width: 400px;
     padding: 20px;
@@ -20,5 +22,10 @@
     background: rgba(0, 0, 0, 0.5);
     width: 100%;
     height: 100%;
+  }
+  h1 {
+    color: #03cfb4;
+    border: none;
+    padding: 0;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// Used to apply styles globally
+import './assets/global.css'
 
 createApp(App).mount('#app')
