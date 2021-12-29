cb491ce18e10d065901eac298c03e496e9860c47
lbertoncello <lucas_b_andrade@hotmail.com>
2021-12-29 17:37:10 -0300

    class: using teleport

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <h1>{{ title }}</h1>
   <p>Welcome...</p>
-  <div v-if="showModal">
+  <!-- 
+    Change the place where the component will be mounted.
+    In this case, #modals instead of #app.
+  -->
+  <teleport to="#modals" v-if="showModal">
     <Modal theme="sale" @close="toggleModal">
       <!-- Named slot -->
       <template v-slot:links>
@@ -13,8 +17,8 @@
       <p>Grab your ninja swag for half price!</p>
 
     </Modal>
-  </div>
-  <div v-if="showModalTwo">
+  </teleport>
+  <teleport to="#modals" v-if="showModalTwo">
     <Modal theme="dark" @close="toggleModalTwo">
       <template v-slot:links>
         <a href="#">Topic 1</a>
@@ -25,7 +29,7 @@
         <p>You just need to choose the topic which you want to know more about</p>
       </div>
     </Modal>
-  </div>
+  </teleport>
   <button @click.alt="toggleModal">Open Modal (alt)</button>
   <button @click.shift="toggleModalTwo">Open Modal (shift)</button>
 </template>
@@ -57,7 +61,7 @@ export default {
 </script>
 
 <style>
-#app {
+#app, .modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
