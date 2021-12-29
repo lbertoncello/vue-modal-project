fcaa5a77027a949d5afe425add4c77895d8c7232
lbertoncello <lucas_b_andrade@hotmail.com>
2021-12-29 15:21:04 -0300

    class: emitting custom events

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <h1>{{ title }}</h1>
-  <Modal :header="header" :text="text" theme="sale" />
+  <p>Welcome...</p>
+  <div v-if="showModal">
+    <Modal :header="header" :text="text" theme="sale" @close="toggleModal" />
+  </div>
+  <button @click="toggleModal">Open Modal</button>
 </template>
 
 <script>
@@ -15,9 +19,15 @@ export default {
     return {
       title: 'My First Vue App :)',
       header: 'Sign up for the Giveaway!',
-      text: 'Grab your ninja swag for half price!'
+      text: 'Grab your ninja swag for half price!',
+      showModal: false
     }
   },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    }
+  }
 }
 </script>
 
